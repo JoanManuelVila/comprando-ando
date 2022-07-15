0e331ba637329caf82696c915dbc7abf5bb2edf5
Joan Vila <vilajoan96@gmail.com>
2022-07-15 13:15:48 -0300

    compando ando

---
+++ src/component/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function Cart() {
+  return (
+    <>
+      <div className='cart'>
+
+      </div>
+    </>
+  )
+}
+
+export default Cart
